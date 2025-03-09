d8aa1562798c8efdde8ccea49ff9ee5134432b1f
Isacco Signori <47895625+isaccoSignori@users.noreply.github.com>
2025-03-09 22:27:36 +0100

    start project again using expo

---
+++ components/Card/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { COLORS } from "@/constants/Colors";
+import { Text, View, TouchableOpacity } from "react-native";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+import { Star, Heart } from "lucide-react-native";
+import { styles } from "./styles";
+
+interface CardProps {
+  data: {
+    image: string;
+    stars: number;
+    name: string;
+    country: string;
+    description: string;
+  };
+  onPress: () => void;
+  fullWidth?: boolean;
+}
+
+export const Card = ({ data, onPress, fullWidth }: CardProps) => {
+  const [isFavourite, setIsFavourite] = useState(false);
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        fullWidth ? styles.fullWidth : styles.cardWidth,
+      ]}
+      onPress={onPress}
+      activeOpacity={0.9}
+    >
+      <Image
+        source={{ uri: data.image }}
+        style={styles.image}
+        contentFit="cover"
+        transition={300}
+      />
+      <LinearGradient
+        colors={["transparent", "rgba(0,0,0,0.7"]}
+        style={styles.gradient}
+      />
+      <View style={styles.content}>
+        <View style={styles.topRow}>
+          <View style={styles.ratingContainer}>
+            <Star size={16} color={COLORS.tertiary} fill={COLORS.tertiary} />
+            <Text style={styles.rating}>{data.stars.toFixed(1)}</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => setIsFavourite((prevState) => !prevState)}
+            style={styles.favoriteButton}
+          >
+            <Heart
+              size={22}
+              color={COLORS.textWhite}
+              fill={isFavourite ? COLORS.primary : "transparent"}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.country}>{data.country}</Text>
+          <Text style={styles.description} numberOfLines={2}>
+            {data.description}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
