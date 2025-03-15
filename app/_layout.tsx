7d58019072c9cb99a075544f754276163840b4bc
Isacco Signori <47895625+isaccoSignori@users.noreply.github.com>
2025-03-15 14:34:06 +0100

    Cheanged tabs + new design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,63 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Compass, MapPinPlus, User } from "lucide-react-native";
+import { COLORS } from "@/constants/Colors";
+import { useTranslation } from "react-i18next";
 
 export default function RootLayout() {
+  const { t } = useTranslation();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.textLight,
+        tabBarStyle: {
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          elevation: 0,
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+        },
+        tabBarIconStyle: {
+          marginBottom: 5,
+        },
+        tabBarLabelPosition: "below-icon",
+        headerStyle: {
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 1,
+          borderBottomColor: COLORS.border,
+        },
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 18,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: t("TABS.HOME"),
+          tabBarIcon: ({ size, color }) => <Compass size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(tabs)/add"
+        options={{
+          title: t("TABS.ADD"),
+          tabBarIcon: ({ size, color }) => <MapPinPlus size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(tabs)/profile"
+        options={{
+          title: t("TABS.PROFILE"),
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
+    </Tabs>
   );
 }
